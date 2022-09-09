71b54bdbce5cf23f5e09adf6e348f6b4e82fde91
chocho77 <chavmom@gmail.com>
2022-09-09 07:00:06 +0300

    Update UserRepository.java

--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/repository/UserRepository.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.codewithchavdar.fullstackbackend.repository;
 
+import com.codewithchavdar.fullstackbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
+    
 }
