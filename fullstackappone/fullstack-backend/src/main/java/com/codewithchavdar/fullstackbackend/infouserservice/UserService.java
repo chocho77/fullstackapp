8a17f319c539fa2733c889d1ae61033006190a4a
chocho77 <chavmom@gmail.com>
2022-09-09 09:46:10 +0300

    add new funcionality
    add Controller
    add Autowired
    into Repository

--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
@@ -1,4 +1,11 @@
 package com.codewithchavdar.fullstackbackend.infouserservice;
 
+import com.codewithchavdar.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
 }
