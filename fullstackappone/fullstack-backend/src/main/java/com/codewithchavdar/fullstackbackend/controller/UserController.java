31acd975264f015e32fb3998d2dc06210ab7ddc2
chocho77 <chavmom@gmail.com>
2022-09-09 14:38:32 +0300

    Update UserController.java

--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.codewithchavdar.fullstackbackend.controller;
 import com.codewithchavdar.fullstackbackend.infouserservice.UserService;
 import com.codewithchavdar.fullstackbackend.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
