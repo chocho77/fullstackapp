fb9b5c42b7ebda85db805f690023fd3579bb00fb
chocho77 <chavmom@gmail.com>
2022-09-09 07:01:26 +0300

    add buisness logic
    add class service

---
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
@@ -0,0 +1,4 @@
+package com.codewithchavdar.fullstackbackend.infouserservice;
+
+public class UserService {
+}
--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/repository/UserRepository.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.codewithchavdar.fullstackbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    
+
 }
