6ed9ca5988016ca66c35f24dd764ddb83130f27e
chocho77 <chavmom@gmail.com>
2022-09-09 10:32:02 +0300

    Update UserService.java

--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
@@ -3,7 +3,9 @@ package com.codewithchavdar.fullstackbackend.infouserservice;
 import com.codewithchavdar.fullstackbackend.model.User;
 import com.codewithchavdar.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
