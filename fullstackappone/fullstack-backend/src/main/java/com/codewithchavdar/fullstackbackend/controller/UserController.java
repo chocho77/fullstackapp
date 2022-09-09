f87f1a077ecb2379d5c3fe9043ee9d417692634e
chocho77 <chavmom@gmail.com>
2022-09-09 10:06:22 +0300

    create new method

--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.codewithchavdar.fullstackbackend.controller;
 
+import com.codewithchavdar.fullstackbackend.infouserservice.UserService;
+import com.codewithchavdar.fullstackbackend.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    UserService userService;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+
+        return userService.save(newUser);
+
+    }
 
 
 
--- fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
+++ fullstackappone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/infouserservice/UserService.java
@@ -1,5 +1,6 @@
 package com.codewithchavdar.fullstackbackend.infouserservice;
 
+import com.codewithchavdar.fullstackbackend.model.User;
 import com.codewithchavdar.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,6 +9,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    
+
+    public User save(User newUser){
+
+        return userRepository.save(newUser);
+
+    }
 
 }
