a8dcd930de01d09ac94054fdbfe14e6394ad7b4d
chocho77 <chavmom@gmail.com>
2022-09-09 12:49:42 +0300

    Update Navbar.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/layout/Navbar.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/layout/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </nav>
