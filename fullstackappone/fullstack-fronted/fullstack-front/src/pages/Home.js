5c532135af1792d8a1b3e77685713d9b67f77cf1
chocho77 <chavmom@gmail.com>
2022-09-09 14:55:17 +0300

    Update Home.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/pages/Home.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/pages/Home.js
@@ -29,12 +29,17 @@ export default function Home() {
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+    {
+        users.map((user,index)=>(
+        <tr>
+            <th scope="row" key={index}>{index+1}</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        ))
+    }
+  
   </tbody>
 </table>
         </div>
