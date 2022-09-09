ddcf0d7e620411ca8561d6a74200bf52be15ad80
chocho77 <chavmom@gmail.com>
2022-09-09 13:13:53 +0300

    Update Navbar.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/layout/Navbar.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/layout/Navbar.js
@@ -9,6 +9,8 @@ export default function Navbar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
+
+                    <button className="btn btn-outline-light">Add User</button>
                 </div>
             </nav>
         </div>
