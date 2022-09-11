826d3c09155c1e530d15eb7fa84b520a4637f909
chocho77 <chavmom@gmail.com>
2022-09-11 06:33:34 -0400

    add interface UserRepository

---
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codewithchavdar.fullstackbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+    
+}
