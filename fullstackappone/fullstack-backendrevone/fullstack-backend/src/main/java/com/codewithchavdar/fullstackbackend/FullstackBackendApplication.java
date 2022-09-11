84948079c516415cccea8da8e05989499948cf52
chocho77 <chavmom@gmail.com>
2022-09-11 12:07:32 +0300

    add new project

--- fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/FullstackBackendApplication.java
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/FullstackBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.codewithchavdar.fullstackbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FullstackBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FullstackBackendApplication.class, args);
 	}
 
 }
