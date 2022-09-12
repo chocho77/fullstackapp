aa23e0496fa5b49c725e38767710b18329d23dce
chocho77 <chavmom@gmail.com>
2022-09-12 16:46:39 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/userservice/UserService.java

--- fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/userservice/UserService.java
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/userservice/UserService.java
@@ -1,5 +1,7 @@
 package com.codewithchavdar.fullstackbackend.userservice;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +18,7 @@ public class UserService {
         return userRepository.save(newUser);
     }
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
