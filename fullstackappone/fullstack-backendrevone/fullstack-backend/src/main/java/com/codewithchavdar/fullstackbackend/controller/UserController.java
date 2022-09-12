2c387366d84e389903ac984e697ee9abcdd03353
chocho77 <chavmom@gmail.com>
2022-09-12 17:18:48 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-backendrevone/fullstack-backend                /src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java

--- fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.codewithchavdar.fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codewithchavdar.fullstackbackend.model.User;
 import com.codewithchavdar.fullstackbackend.userservice.UserService;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class UserController {
 
