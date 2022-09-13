882b20fa25d06ddd98bc2bf8992eebc994f9be6b
chocho77 <chavmom@gmail.com>
2022-09-13 12:09:37 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
@@ -22,8 +22,7 @@ export default function AddUser() {
     const onSubmit=async (e)=>{
         e.preventDefault();
         await axios.post("http://localhost:8080/user")
-
-
+        
     }
 
     return (
