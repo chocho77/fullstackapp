0085acd62a5415cd14f2afd4a7e2db838fe2341c
chocho77 <chavmom@gmail.com>
2022-09-13 10:45:48 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function AddUser() {
     return (
-        <div>AddUser Form</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+                    
+                </div>
+
+            </div>
+        </div>
     )
 }
