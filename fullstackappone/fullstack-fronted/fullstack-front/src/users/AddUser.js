5beb96e79800c1ebbd42bd9a2d78e3bb0fbadd74
chocho77 <chavmom@gmail.com>
2022-09-13 12:00:16 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
@@ -25,7 +25,7 @@ export default function AddUser() {
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                     <h2 className="text-center m-4">Register User</h2>
-                  <form>
+                  <form onSubmit={(e)=>onSubmit(e)}>
                     <div className="mb-3">
                          <label htmlFor="Name" clasName="form-label">
                              Name
