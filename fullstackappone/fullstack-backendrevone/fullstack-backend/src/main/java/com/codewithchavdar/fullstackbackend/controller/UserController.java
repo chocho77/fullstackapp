493a2968dfe8ffdbef6b9fcde2c70217ec8ea1c4
chocho77 <chavmom@gmail.com>
2022-09-13 13:45:50 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-backendrevone/fullstack-backend/src/     main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java

--- fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,11 @@ public class UserController {
     @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userService.updateUser(newUser, id);
     }
 
 }
