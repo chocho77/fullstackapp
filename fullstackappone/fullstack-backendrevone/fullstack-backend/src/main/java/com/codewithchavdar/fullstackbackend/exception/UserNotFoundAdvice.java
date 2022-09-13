342c66d691a22aec4446e97cc0b8ced651e084fc
chocho77 <chavmom@gmail.com>
2022-09-13 13:14:35 +0300

    Changes to be committed: 	new file:   fullstackappone/fullstack-backendrevone/fullstack-backend/src     /main/java/com/codewithchavdar/fullstackbackend/exception/UserNotFoundAdvice.java

---
+++ fullstackappone/fullstack-backendrevone/fullstack-backend/src/main/java/com/codewithchavdar/fullstackbackend/exception/UserNotFoundAdvice.java
@@ -0,0 +1,20 @@
+package com.codewithchavdar.fullstackbackend.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class UserNotFoundAdvice {
+
+    public Map<String, String> exceptionHandler(UserNotFoundException exception) {
+
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", exception.getMessage());
+
+        return errorMap;
+
+    }
+
+}
