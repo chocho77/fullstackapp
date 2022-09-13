329499faf473ffb3969d5d558a1009d28c917ea7
chocho77 <chavmom@gmail.com>
2022-09-13 12:14:28 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js

--- fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
+++ fullstackappone/fullstack-fronted/fullstack-front/src/users/AddUser.js
@@ -1,5 +1,6 @@
-import axios from "axios"
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
 
 export default function AddUser() {
 
@@ -9,7 +10,7 @@ export default function AddUser() {
         name:"",
         username:"",
         email:""
-    })
+    });
 
     const{name,username,email}=user
 
