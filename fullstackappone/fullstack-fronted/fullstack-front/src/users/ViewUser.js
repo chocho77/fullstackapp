04e0b697b5c8bf8235b5c9c313f76032bc46b571
chavdar77 <chavmom@gmail.com>
2022-10-17 03:33:45 +0300

    Changes to be committed: 	modified:   fullstackappone/fullstack-fronted/fullstack-front/src/users/ViewUser.js

---
+++ fullstackappone/fullstack-fronted/fullstack-front/src/users/ViewUser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ViewUser() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className="text-center m-4">Register user</h2>
+                </div>
+            </div>
+        </div>
+
+    );
+}
