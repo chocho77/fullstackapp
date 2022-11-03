b359f75f420ca686936031a9779a1201888ad167
chavdar77 <chavmom@gmail.com>
2022-11-03 18:10:33 +0200

    Changes to be committed: 	new file:   fullstackangularspring/backend/employeemanager/.gitignore 	new file:   fullstackangularspring/backend/employeemanager/.mvn/wrapper/maven-wrapper.jar 	new file:   fullstackangularspring/backend/employeemanager/.mvn/wrapper/maven-wrapper.properties 	new file:   fullstackangularspring/backend/employeemanager/mvnw 	new file:   fullstackangularspring/backend/employeemanager/mvnw.cmd 	new file:   fullstackangularspring/backend/employeemanager/pom.xml 	new file:   fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/EmployeemanagerApplication.java 	modified:   fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/model/Employee.java 	new file:   fullstackangularspring/backend/employeemanager/src/main/resources/application.properties 	new file:   fullstackangularspring/backend/employeemanager/src/test/java/com/netcho/employeemanager/EmployeemanagerApplicationTests.java 	new file:   fullstackangularspring/backend/origin/employeemanager.zip

--- fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/model/Employee.java
+++ fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/model/Employee.java
@@ -1,2 +1,20 @@
-package com.netcho.employeemanager.model;public class Employee {
+package com.netcho.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String employeeCode;
+
 }
