5aad7d46c7f1507535448525fb1bc76cc24206b3
chavdar77 <chavmom@gmail.com>
2022-11-04 07:19:22 +0200

    Changes to be committed: 	modified:   fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/service/EmployeeService.java

--- fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/service/EmployeeService.java
+++ fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/service/EmployeeService.java
@@ -1,4 +1,23 @@
 package com.netcho.employeemanager.service;
 
+import com.netcho.employeemanager.model.Employee;
+import com.netcho.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
 }
