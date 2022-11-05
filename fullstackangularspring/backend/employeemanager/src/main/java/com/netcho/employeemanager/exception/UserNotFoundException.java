42b42d0837524cac7b2dbf133ea0d51358abea83
chavdar77 <chavmom@gmail.com>
2022-11-05 14:02:23 +0200

    Changes to be committed: 	modified:   fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/exception/UserNotFoundException.java 	modified:   fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/repository/EmployeeRepository.java

--- fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/exception/UserNotFoundException.java
+++ fullstackangularspring/backend/employeemanager/src/main/java/com/netcho/employeemanager/exception/UserNotFoundException.java
@@ -2,5 +2,6 @@ package com.netcho.employeemanager.exception;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message) {
+        super(message);
     }
 }
