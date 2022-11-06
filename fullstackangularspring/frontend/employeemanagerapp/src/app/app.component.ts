c3c8c802a9262241673aa490623c667203001e96
chavdar77 <chavmom@gmail.com>
2022-11-06 06:26:40 +0200

    Changes to be committed: 	modified:   fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts

--- fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts
+++ fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,23 @@ import { Employee } from './employee';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public employees:Employee[];
+  public employees: Employee[] = [];
+
+  constructor(private employeeService:EmployeeService){}
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    );
+
+  }
+
 
   
 }
