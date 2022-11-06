913d9216e32a0f445bce6ce46c2c72d70d1e89aa
chavdar77 <chavmom@gmail.com>
2022-11-06 03:52:08 +0200

    Changes to be committed: 	modified:   fullstackangularspring/frontend/employeemanagerapp/src/app/employee.service.ts

--- fullstackangularspring/frontend/employeemanagerapp/src/app/employee.service.ts
+++ fullstackangularspring/frontend/employeemanagerapp/src/app/employee.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
