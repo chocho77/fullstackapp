971311f0108e39ad130634d33057f8b085ed6196
chavdar77 <chavmom@gmail.com>
2022-11-08 04:55:58 +0200

    Changes to be committed: 	modified:   fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.html 	modified:   fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts

--- fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts
+++ fullstackangularspring/frontend/employeemanagerapp/src/app/app.component.ts
@@ -32,6 +32,16 @@ export class AppComponent implements OnInit{
     const button = document.createElement('button');
     button.type = 'button';
     button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    if(mode === 'add'){
+      button.setAttribute('data-target', 'addEmployeeModal');
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', 'updateEmployeeModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', 'deleteEmployeeModal');
+    }
 
   }
 
